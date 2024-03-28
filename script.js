e5965f737fef2423208a43c0eb880bec8665fb02
Rolly Maulana Awangga <awangga@gmail.com>
2024-03-28 13:24:49 +0700

    taskId

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function dragDrop(e) {
     this.classList.remove('over');
     const taskId = e.dataTransfer.getData('text/plain');
     const task = document.getElementById(taskId);
-    console.log(task);
+    console.log(taskId);
     if (task && this.classList.contains('kanban-column')) {
         const columnId = this.id; // Get the column ID where the task is dropped
         console.log(columnId);
